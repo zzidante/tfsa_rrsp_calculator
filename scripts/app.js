f6cfeff18e84defa0c6426e6cbae8fd7e1b6bcdb
Mel <zzidante@gmail.com>
2017-12-21 02:09:30 +0000

    formatted floats to display two decimals, render testData to page w jQuery

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 // __equations is self-made library imported into index.html (global scope)
 // I chose this method to allow 'modules' without needing to run a server to compile ES6 syntax into ES5.
-var equations = __equations  
+var equations = __equations;
 
 $(function() {
   var form = $('#financial-calculator');  
@@ -20,7 +20,7 @@ $(function() {
     fvRrsp: 0,
     taxRrsp: 0,
     afterTaxFvRrsp: 0
-  }
+  };
 
   // datahelper functions 
   function updateAccount(account, userKey, update) {
@@ -35,29 +35,65 @@ $(function() {
 
   function getFvTfsa(user) {
     return equations.futureValue(user.tfsaDeposit, user.interestRate, user.inflationRate, user.yearsToInvest);
-  }
+  };
 
   function getFvRrsp(user) {
     return equations.futureValue(user.depositAmount, user.interestRate, user.inflationRate, user.yearsToInvest);
-  }
+  };
 
   function getRrspTaxAmount(user) {
     return equations.rrspTaxAmount(user.fvRrsp, user.retireTaxRate);
-  }
+  };
 
   function getRrspAfterTaxFv(user) {
     return equations.getRrspAfterTaxFv(user.fvRrsp, user.retireTaxRate);
+  };
+
+  // jQuery executables
+
+  function lookupKeyValueResponse(objKey) {
+    if (objKey === 'depositAmount') {
+      return "Your original RRSP deposit is $";
+    } else if (objKey === 'tfsaDeposit') {
+      return "Your total TFSA deposit (before tax) is $";
+    } else if (objKey === 'fvTfsa') {
+      return "The Future Value of your TFSA is $";
+    } else if (objKey === 'fvRrsp') {
+      return "The Future Value of your RRSP is $";
+    } else if (objKey === 'taxRrsp') {
+      return "The taxes paid on your RRSP during retirement are $";
+    } else if (objKey === 'afterTaxFvRrsp') {
+      return "The total take-home amount of your RRSP's are $";
+    } else {
+      return false;
+    }
   }
 
-    // run operations in order...
+  function renderResults(user) {
+    user.forEach(function(value, index) {
+      resultsContainer.append(value);
+    });
+  }
+
+  function renderAllInformation(user) {
+    var userInfo = [];
+    $.each(user, function( key, value ) {
+      if (lookupKeyValueResponse(key)) {
+        userInfo.push('<p class="finance-calc-result">' + lookupKeyValueResponse(key) + value + '</p>');
+      }
+    });
+    renderResults(userInfo);
+  }
+
+  // run operations in order...
   function runFinalComputation(user) {
     updateAccount(user, "tfsaDeposit", updateTfsaDeposit(user));
     updateAccount(user, "fvTfsa", getFvTfsa(user));
     updateAccount(user, "fvRrsp", getFvRrsp(user));
     updateAccount(user, "taxRrsp", getRrspTaxAmount(user));
     updateAccount(user, "afterTaxFvRrsp", getRrspAfterTaxFv(user));
-    console.log(user.fvTfsa, user.afterTaxFvRrsp);
-  } 
-  
+
+    renderAllInformation(user);
+  };
   runFinalComputation(testData);
 });
--- scripts/modules/financialEquations.js
+++ scripts/modules/financialEquations.js
@@ -1,17 +1,22 @@
 // Reference: https://code.tutsplus.com/tutorials/build-your-first-javascript-library--net-26796 
-window.__equations = (function () {    
+window.__equations = (function () { 
+  
+  function formatFloat(floatNum) {
+    return parseFloat(Math.round(floatNum * 100) / 100).toFixed(2);
+  }
+
   var __equations = {
     afterTaxDeposit: function (deposit, taxRate) {
-    return deposit + (deposit * taxRate);
+    return formatFloat(deposit + (deposit * taxRate));
     },
     futureValue: function (deposit, interestRate, inflationRate, yearsToInvest) {
-      return deposit * ((1 + (interestRate - inflationRate))**yearsToInvest);
+      return formatFloat(deposit * ((1 + (interestRate - inflationRate))**yearsToInvest));
     },
     rrspTaxAmount: function (futureValue, retireTaxRate) {
-      return futureValue * retireTaxRate;
+      return formatFloat(futureValue * retireTaxRate);
     },
     getRrspAfterTaxFv: function (futureValue, retireTaxRate) {
-      return futureValue - (futureValue * retireTaxRate);
+      return formatFloat(futureValue - (futureValue * retireTaxRate));
     }
   };
   return __equations;
